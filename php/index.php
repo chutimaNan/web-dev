bde02fc7d876e295d952eeb3fc89040f9c2665b5
chutimaNan <52516526+chutimaNan@users.noreply.github.com>
2020-09-28 22:53:24 +0700

    d

--- php/index.php
+++ php/index.php
@@ -1,3 +1,22 @@
+<?php
+
+echo 'Hello from Docker2';
+
+$host = 'db'; // service name from docker-compose.yml
+$user = 'devuser';
+$password = 'devpass';
+$db = 'test_db';
+
+$conn = new mysqli($host,$user,$password,$db);
+if($conn->connect_error){
+    echo 'connection failed' . $conn->connection_error;
+}
+echo 'Successfully connected to MYSQL';
+?>
+
+
+
+
 <!doctype html>
 <html class="no-js" lang="zxx">
     <head>
